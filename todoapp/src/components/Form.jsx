72b0fb66ee746257b3dea11130560919d05ba315
mervinsula <90396724+mervinsula@users.noreply.github.com>
2024-04-02 13:18:58 +0800

    Add functionality

--- todoapp/src/components/Form.jsx
+++ todoapp/src/components/Form.jsx
@@ -3,13 +3,14 @@ import styles from './form.module.css';
 
 export default function Form({todos, setTodos}) {
 
-    const [todo, setTodo] = useState("");
+    //const [todo, setTodo] = useState("");
+    const [todo, setTodo] = useState({name: "", done: false});
 
 
     function handleSubmit(e) {
         e.preventDefault();
         setTodos([...todos,todo]);
-        setTodo("");
+        setTodo({name: "", done: false});
     }
     
     return (
@@ -18,8 +19,8 @@ export default function Form({todos, setTodos}) {
         <div className={styles.inputContainer}>
             <input 
                 className={styles.modernInput} 
-                onChange={(e)=>setTodo(e.target.value)} 
-                value={todo} 
+                onChange={(e)=>setTodo({name: e.target.value, done: false})} 
+                value={todo.name} 
                 type="text"
                 placeholder="Enter todo item.."/>
             <button className={styles.modernButton} type="submit">Add</button>
